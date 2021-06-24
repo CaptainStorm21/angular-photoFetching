969e9345288fa50e5be6d5bdd97da23dada7a2cf
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-06-24 02:34:53 -0700

    Generated service and new component photos show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoFetchingService } from './services/photo-fetching.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-photoFetching';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PhotoShowComponent } from './photo-show/photo-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoShowComponent
   ],
   imports: [
     BrowserModule,
