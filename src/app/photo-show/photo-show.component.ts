f9479b785fbc751234a221662dbef70218ce4ddb
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-06-28 01:18:28 -0700

    making a request

--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// step 1 dependency injection
+import { PhotoFetchingService } from '../services/photo-fetching.service';
 
 @Component({
   selector: 'app-photo-show',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoShowComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    // step 2 - dependency injection
+    private  photoFetchingService: PhotoFetchingService
+  ) {
+    // step 3 - call getPhoto() method
+    // Step 4  - open up a network tab -> then to network request -> response header
+    // step 4 = in subscription we added, get access t the response and console log it
+    // step 4.1 - console.log (urls.regular)
+    this.photoFetchingService.getPhoto().subscribe(() => { });
+  }
 
   ngOnInit(): void {
   }
